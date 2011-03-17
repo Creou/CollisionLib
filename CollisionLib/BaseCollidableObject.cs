6b7de6d2c77201abc9bda666c279694a40b1aaef
Simon P Stevens <simonpstevens@gmail.com>
2011-03-17 20:26:22 +0000

    Extracted common logic to BaseCollidableObject class

---
+++ CollisionLib/BaseCollidableObject.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CollisionLib
+{
+    public abstract class BaseColliableObject : DrawableGameComponent, ICollidable
+    {
+        private String _textureName;
+        private Texture2D _texture;
+
+        private SpriteBatch _spriteBatch;
+
+        private float _friction;
+        private float _scale;
+
+        private Vector2 _position;
+        private Vector2 _velocity;
+        private float _mass;
+
+        public float Mass { get { return _mass; } protected set { _mass = value; } }
+        public Vector2 Position { get { return _position; } protected set { _position = value; } }
+        public Vector2 Velocity { get { return _velocity; } protected set { _velocity = value; } }
+
+        public float Scale { get { return _scale; } protected set { _scale = value; } }
+        public float Friction { get { return _friction; } protected set { _friction = value; } }
+        public String TextureName { get { return _textureName; } protected set { _textureName = value; } }
+        public Texture2D Texture { get { return _texture; } protected set { _texture = value; } }
+
+        public BaseColliableObject(Game game) : base(game) { }
+
+        public BaseColliableObject(Game game, String textureName, float scale, float mass, Vector2 initialPosition, Vector2 initialVelocity, float friction) :
+            base(game)
+        {
+            TextureName = textureName;
+            Scale = scale;
+            Mass = mass;
+            Position = initialPosition;
+            Velocity = initialVelocity;
+            Friction = 0.001f;
+        }
+
+        public void SetState(Vector2 newPosition, Vector2 newVelocity)
+        {
+            _position = newPosition;
+            _velocity = newVelocity;
+        }
+
+        public void SetPosition(Vector2 newPosition)
+        {
+            SetState(newPosition, Velocity);
+        }
+
+        public void SetVelocity(Vector2 newVelocity)
+        {
+            SetState(Position, newVelocity);
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(this.Game.GraphicsDevice);
+
+            Texture = this.Game.Content.Load<Texture2D>(TextureName);
+
+            base.LoadContent();
+        }
+
+        private void ApplyFriction()
+        {
+            Vector2 velocityNormalized = Velocity;
+            velocityNormalized.Normalize();
+
+            float adjustedLength = (Velocity.Length() - _friction);
+            if (adjustedLength > 0)
+            {
+                Velocity = velocityNormalized * adjustedLength;
+            }
+            else
+            {
+                Velocity = new Vector2(0, 0);
+            }
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            float newPositionX = (float)(Position.X + (Velocity.X * gameTime.ElapsedGameTime.TotalMilliseconds));
+            float newPositionY = (float)(Position.Y + (Velocity.Y * gameTime.ElapsedGameTime.TotalMilliseconds));
+
+            Position = new Vector2(newPositionX, newPositionY);
+
+            ApplyFriction();
+
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(_texture, Position, null, Color.White, 0, new Vector2(_texture.Width / 2, _texture.Height / 2), _scale, SpriteEffects.None, 0);
+
+            base.Draw(gameTime);
+
+            _spriteBatch.End();
+        }
+    }
+}
--- CollisionLibDemo/CollisionLibDemo/Ball.cs
+++ CollisionLibDemo/CollisionLibDemo/Ball.cs
@@ -9,24 +9,11 @@ using CollisionLib;
 
 namespace CollisionDetectionDemo
 {
-    public class Ball : DrawableGameComponent, ICollidableCircle
+    public class Ball : BaseColliableObject, ICollidableCircle
     {
-        private Texture2D _ballTexture;
-        private SpriteBatch _spriteBatch;
+        public float Diameter { get { return Texture.Width * Scale; } }
 
-        private Vector2 _position;
-        private Vector2 _velocity;
-        private float _friction;
-
-        private float _scale;
-        private float _mass;
-
-        public Vector2 Position { get { return _position; } }
-        public Vector2 Velocity { get { return _velocity; } }
-        public float Mass { get { return _mass; } }
-        public float Diameter { get { return _ballTexture.Width * _scale; } }
-
-        private String _texture;
+        
 
         public Ball(Game game, String texture, float scale, float mass)
             : this(game, texture, scale, mass, Vector2.Zero, Vector2.Zero)
@@ -34,75 +21,8 @@ namespace CollisionDetectionDemo
         }
 
         public Ball(Game game, String texture, float scale, float mass, Vector2 initialPosition, Vector2 initialVelocity)
-            : base(game)
+            : base(game, texture, scale, mass, initialPosition, initialVelocity, 0.001f)
         {
-            _texture = texture;
-            _scale = scale;
-            _mass = mass;
-            _position = initialPosition;
-            _velocity = initialVelocity;
-            _friction = 0.001f;
-        }
-
-        public void SetState(Vector2 newPosition, Vector2 newVelocity)
-        {
-            _position = newPosition;
-            _velocity = newVelocity;
-        }
-
-        public void SetPosition(Vector2 newPosition)
-        {
-            SetState(newPosition, Velocity);
-        }
-
-        public void SetVelocity(Vector2 newVelocity)
-        {
-            SetState(Position, newVelocity);
-        }
-
-        protected override void LoadContent()
-        {
-            _spriteBatch = new SpriteBatch(this.Game.GraphicsDevice);
-
-            _ballTexture = this.Game.Content.Load<Texture2D>(_texture);
-
-            base.LoadContent();
-        }
-
-        public override void Update(GameTime gameTime)
-        {
-            _position.X = (float)(_position.X + (_velocity.X * gameTime.ElapsedGameTime.TotalMilliseconds));
-            _position.Y = (float)(_position.Y + (_velocity.Y * gameTime.ElapsedGameTime.TotalMilliseconds));
-            ApplyBallFriction();
-
-            base.Update(gameTime);
-        }
-
-        private void ApplyBallFriction()
-        {
-            Vector2 velocityNormalized = _velocity;
-            velocityNormalized.Normalize();
-
-            float adjustedLength = (_velocity.Length() - _friction);
-            if (adjustedLength > 0)
-            {
-                _velocity = velocityNormalized * adjustedLength;
-            }
-            else
-            {
-                _velocity = new Vector2(0, 0);
-            }
-        }
-
-        public override void Draw(GameTime gameTime)
-        {
-            _spriteBatch.Begin();
-
-            _spriteBatch.Draw(_ballTexture, _position, null, Color.White, 0, new Vector2(_ballTexture.Width / 2, _ballTexture.Height / 2), _scale, SpriteEffects.None, 0);
-
-            base.Draw(gameTime);
-
-            _spriteBatch.End();
         }
     }
 }
