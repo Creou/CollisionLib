1db223ec6f483ece5beca5d58e3e3e0a60235181
Simon P Stevens <simonpstevens@gmail.com>
2011-03-17 20:27:30 +0000

    Added interfaces and demo object for a collidable axis aligned rectangle

---
+++ CollisionLib/IColliadableAxisAlignedRectangle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CollisionLib
+{
+    public interface IColliadableAxisAlignedRectangle
+    {
+        float Width { get; }
+        float Height { get; }
+    }
+}
---
+++ CollisionLibDemo/CollisionLibDemo/Square.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CollisionLib;
+using Microsoft.Xna.Framework;
+
+namespace CollisionDetectionDemo
+{
+    class Square : BaseColliableObject, IColliadableAxisAlignedRectangle
+    {
+        public float Height { get { return Texture.Height * Scale; } }
+        public float Width { get { return Texture.Width * Scale; } }
+
+        public Square(Game game, String texture, float scale, float mass)
+            : this(game, texture, scale, mass, Vector2.Zero, Vector2.Zero)
+        {
+        }
+
+        public Square(Game game, String texture, float scale, float mass, Vector2 initialPosition, Vector2 initialVelocity)
+            : base(game, texture, scale, mass, initialPosition, initialVelocity, 0.001f)
+        {
+        }
+    }
+}
